2db324af4b45a38bc0fe65376ab8e280e96cdcca
kobeisgood <theo.khademi@gmail.com>
2019-05-23 01:04:13 +0200

    update wizardDeliveryController, wizard_delivery(fxml)

--- src/controls/WizardDeliveryController.java
+++ src/controls/WizardDeliveryController.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import model.WizardStage;
@@ -13,6 +14,8 @@ import model.Profile;
 
 import java.io.IOException;
 import java.util.function.UnaryOperator;
+
+import static java.lang.Integer.parseInt;
 
 public class WizardDeliveryController extends AnchorPane {
 
@@ -72,12 +75,15 @@ public class WizardDeliveryController extends AnchorPane {
     Button wizardToPaymentButton;
     @FXML
     Button wizardDeliverySaveButton;
+    @FXML
+    Button wizardDeliverySavedButton;
 
     private static WizardDeliveryController instance;
     private WizardStageController parentController;
-    ToggleGroup dateSelected = new ToggleGroup();
-    ToggleGroup typeOfHousing = new ToggleGroup();
-    Profile profile;
+    private ToggleGroup dateSelected = new ToggleGroup();
+    private ToggleGroup typeOfHousing = new ToggleGroup();
+    private Profile profile;
+    StringBuilder sb;
 
     private WizardDeliveryController() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/wizard_delivery.fxml"));
@@ -89,6 +95,114 @@ public class WizardDeliveryController extends AnchorPane {
                 IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        wizardApartment.setSelected(true);
+        profile = Profile.getInstance();
+        sb = new StringBuilder();
+
+        initTextFormatters();
+        addChangeListeners();
+        initToggleGroups();
+        initWizardProfileForm();
+
+        enableToPaymentButton(false);
+        wizardDeliverySaveButton.setVisible(false);
+        wizardDeliverySavedButton.setVisible(false);
+
+    }
+
+    public static WizardDeliveryController getInstance() {
+        if (instance == null)
+            instance = new WizardDeliveryController();
+        return instance;
+    }
+
+    public void setParentController(WizardStageController controller) {
+        parentController = controller;
+    }
+
+    public void refresh() {
+        initWizardProfileForm();
+    }
+
+    @FXML
+    private void toPaymentStage() {
+        parentController.viewPaymentStage();
+    }
+
+    @FXML
+    private void toCartStage() {
+        parentController.viewCartStage();
+    }
+
+    private void initWizardProfileForm() {
+        profile = FilesBackend.getInstance().readProfileFromFile();
+        if(profile == null){ return; }
+        if (profile != null) {
+            wizardFirstName.setText(profile.getFirstName());
+            wizardLastName.setText(profile.getLastName());
+            wizardPhoneNumber.setText(profile.getMobilePhoneNumber());
+            wizardAdress.setText(profile.getAddress());
+            wizardCity.setText(profile.getCity());
+            wizardZipCode.setText(profile.getPostCode());
+            wizardLevel.setText(Integer.toString(profile.getLevel()));
+        }
+    }
+
+    private void limitTextLength(TextField field, int limit) {
+        field.lengthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                if (newValue.intValue() > oldValue.intValue()) {
+                    if (newValue.intValue() > limit) {
+                        field.setText(field.getText().substring(0, limit));
+
+
+                    } else if (newValue.intValue() == limit) {
+                        field.getStyleClass().clear();
+                        field.getStyleClass().addAll("text-field", "text-input", "text-normal-medium");
+                        if(isValidLength(field,limit))
+                            enableToPaymentButton(true);
+                    }
+
+                } else {
+                    if (newValue.intValue() < limit) {
+                        field.getStyleClass().clear();
+                        field.getStyleClass().addAll("text-field", "text-input", "text-normal-medium", "incorrect-format");
+                        enableToPaymentButton(false);
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void enableToPaymentButton(boolean b) {
+        wizardToPaymentButton.setDisable(!b);
+    }
+
+    private void enableWizardSaveButton(boolean b) {
+        wizardDeliverySaveButton.setVisible(b);
+        wizardDeliverySaveButton.setDisable(!b);
+        wizardDeliverySaveButton.getStyleClass().add("green-button-active");
+    }
+
+    private boolean isValidLength(TextField textField, int limit){
+        return (textField.getText().length() == limit || textField.getText().length() == 0);
+    }
+
+    public void addChangeListener(Node node) {
+        node.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) {
+                if (newPropertyValue)
+                    enableWizardSaveButton(true);
+            }
+        });
+    }
+
+    private void initTextFormatters() {
+        /* Apply text filters on textfields */
 
         UnaryOperator<TextFormatter.Change> onlyDigitsFilter = change -> {
             String text = change.getText();
@@ -114,96 +228,116 @@ public class WizardDeliveryController extends AnchorPane {
         wizardZipCode.setTextFormatter(zipCodeFormat);
         limitTextLength(wizardZipCode, 5);
 
-        updateProfileForm();
-        wizardToPaymentButton.setDisable(true);
-        wizardDeliverySaveButton.setVisible(false);
+        TextFormatter<String> firstNameFormat = new TextFormatter<>(onlyLettersFilter);
+        wizardFirstName.setTextFormatter(firstNameFormat);
 
+        TextFormatter<String> lastNameFormat = new TextFormatter<>(onlyLettersFilter);
+        wizardLastName.setTextFormatter(lastNameFormat);
+
+        TextFormatter<String> cityFormat = new TextFormatter<>(onlyLettersFilter);
+        wizardCity.setTextFormatter(cityFormat);
     }
 
-    public static WizardDeliveryController getInstance() {
-        if (instance == null)
-            instance = new WizardDeliveryController();
-        return instance;
+    private void addChangeListeners(){
+        limitTextLength(wizardZipCode, 5);
+        limitTextLength(wizardPhoneNumber, 10);
+        addChangeListener(wizardFirstName);
+        addChangeListener(wizardLastName);
+        addChangeListener(wizardAdress);
+        //addChangeListener(wizardEmailField);
+        addChangeListener(wizardCity);
+        addChangeListener(wizardHouse);
+        addChangeListener(wizardApartment);
+        addChangeListener(wizardLevel);
     }
 
-    public void setParentController(WizardStageController controller) {
-        parentController = controller;
-    }
-
-    public void refresh() {
-        updateProfileForm();
+    private void initToggleGroups() {
+        wizardApartment.setToggleGroup(typeOfHousing);
+        wizardHouse.setToggleGroup(typeOfHousing);
+        //todo add date buttons togglegroup
     }
 
     @FXML
-    private void toPaymentStage() {
-        parentController.viewPaymentStage();
+    private void wizardHouseSelected() {
+        wizardLevel.setDisable(true);
     }
 
     @FXML
-    private void toCartStage() {
-        parentController.viewCartStage();
+    private void wizardApartmentSelected() {
+        wizardLevel.setDisable(false);
     }
 
-    private void updateProfileForm() {
-        profile = FilesBackend.getInstance().readProfileFromFile();
+    private void updateProfile() {
+        profile.setFirstName(wizardFirstName.getText());
+        profile.setLastName(wizardLastName.getText());
+        profile.setMobilePhoneNumber(wizardPhoneNumber.getText());
+        profile.setAddress(wizardAdress.getText());
+        profile.setCity(wizardCity.getText());
+        profile.setPostCode(wizardZipCode.getText());
+        //profile.setEmail(eMailField.getText());
+        profile.setLevel(parseInt(wizardLevel.getText()));
 
-        if (profile != null) {
-            wizardFirstName.setText(profile.getFirstName());
-            wizardLastName.setText(profile.getLastName());
-            wizardPhoneNumber.setText(profile.getMobilePhoneNumber());
-            wizardAdress.setText(profile.getAddress());
-            wizardCity.setText(profile.getCity());
-            wizardZipCode.setText(profile.getPostCode());
-            wizardLevel.setText(Integer.toString(profile.getLevel()));
+        FilesBackend.getInstance().saveProfile(profile);
+
+    }
+    private void changeToSavedButton(boolean b) {
+        wizardDeliverySaveButton.setVisible(!b);
+        wizardDeliverySavedButton.setVisible(b);
+    }
+
+    @FXML
+    private void wizardDeliveryUpdate() {
+
+        if (allFieldsValid()) {
+            updateProfile();
+            changeToSavedButton(true);
+            sb.setLength(0);
         }
+        //errorMessage.setVisible(true);
+        //errorMessage.setText(sb.toString());
+        sb.setLength(0);
     }
 
-    private void limitTextLength(TextField field, int limit) {
-        field.lengthProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
-                if (newValue.intValue() > oldValue.intValue()) {
-                    if (newValue.intValue() > limit) {
-                        field.setText(field.getText().substring(0, limit));
-                        enableToPaymentButton(true);
-                    } else if (newValue.intValue() == limit) {
-                        field.getStyleClass().clear();
-                        field.getStyleClass().addAll("text-field", "text-input", "text-normal-medium");
-                        if(isValidLength(field,limit))
-                            enableToPaymentButton(true);
-                    }
-                } else {
-                    if (newValue.intValue() < limit) {
-                        field.getStyleClass().clear();
-                        field.getStyleClass().addAll("text-field", "text-input", "text-normal-medium", "incorrect-format");
-                        enableToPaymentButton(false);
-                    }
+    //Bad practice, not following command query principle....
 
-                }
+    private boolean allFieldsValid() {
+        return (validZipCode() && validPhoneNo() && validLevel()); //validEmail &&
+    }
+
+    private boolean validZipCode() {
+        if (wizardZipCode.getText().length() == 5 || wizardZipCode.getText().equals(""))
+            return true;
+        sb.append("ange giltigt postnummer. ");
+        return false;
+    }
+
+
+    private boolean validPhoneNo() {
+        if (wizardPhoneNumber.getText().length() == 10 || wizardPhoneNumber.getText().equals(""))
+            return true;
+        sb.append("ange giltigt mobilnummer. ");
+        return false;
+    }
+
+    private boolean validLevel() {
+        if(wizardLevel.getText().length() > 0) {
+            try {
+                int lev = parseInt(wizardLevel.getText());
+                return (lev < 99);
+            } catch (NumberFormatException e) {
+                if(!wizardLevel.isDisabled())
+                    sb.append("ange giltigt våning. ");
+
             }
-        });
+        }
+        return false;
     }
 
-    private void enableToPaymentButton(boolean b) {
-        wizardToPaymentButton.setDisable(!b);
+    /*private boolean validEmail() {
+        if ((eMailField.getText().contains("@") && eMailField.getText().contains(".")) || eMailField.getText().equals(""))
+            return true;
+        sb.append("ange giltig e-postadress. ");
+        return false;
     }
-
-    private boolean isValidLength(TextField textField, int limit){
-        return (textField.getText().length() == limit || textField.getText().length() == 0);
-    }
-
-    public void addChangeListener(TextField textField) {
-        textField.focusedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue) {
-                if (newPropertyValue)
-                    enableWizardSaveButton(true);
-            }
-        });
-    }
-
-    private void enableWizardSaveButton(boolean b) {
-        wizardDeliverySaveButton.setVisible(true);
-        wizardDeliverySaveButton.setDisable(!b);
-    }
+    */
 }
