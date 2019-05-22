7a0f7c8ae68707e2783adb161dcc68c15da2a42c
Julia Gustafsson <gustafsson.julia@live.se>
2019-05-22 10:37:12 +0200

    Make WizardDeliveryController a Singleton

--- src/controls/WizardDeliveryController.java
+++ src/controls/WizardDeliveryController.java
@@ -1,4 +1,21 @@
 package controls;
 
 public class WizardDeliveryController {
+
+    private static WizardDeliveryController instance;
+    private WizardDeliveryController parentController;
+
+    private WizardDeliveryController() {
+
+    }
+
+    public static WizardDeliveryController getInstance() {
+        if (instance == null)
+            instance = new WizardDeliveryController();
+        return instance;
+    }
+
+    public void setParentController(WizardDeliveryController controller) {
+        parentController = controller;
+    }
 }
