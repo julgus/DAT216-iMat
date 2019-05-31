b6c0a48aa841278cfe16612fe06114f07a23f57f
Julia Gustafsson <gustafsson.julia@live.se>
2019-05-31 13:51:22 +0200

    Visual changes

--- src/model/CartItem.java
+++ src/model/CartItem.java
@@ -1,7 +1,6 @@
 package model;
 
 import javafx.animation.FillTransition;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -13,7 +12,6 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.io.IOException;
-import java.time.Clock;
 
 public class CartItem extends AnchorPane {
 
@@ -54,11 +52,16 @@ public class CartItem extends AnchorPane {
     public void updateLabels(){
         updateNoOfItems();
         updatePrice();
+        ft = new FillTransition(Duration.millis(1500), backgroundRect, Color.web("#F5842C"), Color.WHITE);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
     }
 
     private void updateNoOfItems(){
         String unit = item.getProduct().getUnit().equals("kr/kg") ? " kg" : " st";
         numberOfItems.setText((item.getNumberOfItems()) + unit);
+
     }
     private void updatePrice(){
         cartItemPrice.setText(String.format("%1$,.2f", item.getProduct().getPrice() * item.getNumberOfItems()) + " kr");
